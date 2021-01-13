d1ef90ab4b3b611ef5a22e4ba8ce0210170cff01
kwerghi <k-werghi@artefrance.fr>
2021-01-13 01:36:21 +0100

    Add room availability tests

--- src/main/java/com/kata/demo/core/domain/model/Room.java
+++ src/main/java/com/kata/demo/core/domain/model/Room.java
@@ -1,9 +1,22 @@
 package com.kata.demo.core.domain.model;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Room {
     private String name;
     private List<Booking> bookings;
     private Long version;
+
+    public Room(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public boolean isRoomAvailable(LocalDate arrivalDate, LocalDate departureDate){
+        return false;
+    }
 }
--- src/test/java/com/kata/demo/core/domain/model/RoomTest.java
+++ src/test/java/com/kata/demo/core/domain/model/RoomTest.java
@@ -1,2 +1,65 @@
-package com.kata.demo.core.domain.model;public class RoomTest {
+package com.kata.demo.core.domain.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RoomTest {
+
+    @Test
+    public void shouldReturnsFalse_WhenRoomHaveConflictingBooking(){
+        // Given
+        LocalDate requestedArrivalDate = LocalDate.of(2021, Month.JANUARY, 10);
+        LocalDate requestedDepartureDate = LocalDate.of(2021, Month.JANUARY, 20);
+        Booking booking = mock(Booking.class);
+        when(booking.conflictWith(requestedArrivalDate, requestedDepartureDate)).thenReturn(true);
+        Room room = new Room(Collections.singletonList(booking));
+
+        // When
+        boolean isAvailable = room.isRoomAvailable(requestedArrivalDate, requestedDepartureDate);
+
+        // Then
+        assertFalse(isAvailable);
+    }
+
+    @Test
+    public void shouldReturnsTrue_WhenRoomDoesNotHaveConflictingBooking(){
+        // Given
+        LocalDate requestedArrivalDate = LocalDate.of(2021, Month.JANUARY, 10);
+        LocalDate requestedDepartureDate = LocalDate.of(2021, Month.JANUARY, 20);
+        Booking booking = mock(Booking.class);
+        when(booking.conflictWith(requestedArrivalDate, requestedDepartureDate)).thenReturn(false);
+        Room room = new Room(Collections.singletonList(booking));
+
+        // When
+        boolean isAvailable = room.isRoomAvailable(requestedArrivalDate, requestedDepartureDate);
+
+        // Then
+        assertTrue(isAvailable);
+    }
+
+    @Test
+    public void shouldReturnsTrue_WhenRoomBookingsIsEmpty(){
+        // Given
+        LocalDate requestedArrivalDate = LocalDate.of(2021, Month.JANUARY, 10);
+        LocalDate requestedDepartureDate = LocalDate.of(2021, Month.JANUARY, 20);
+        Room room = new Room(new ArrayList<>());
+
+        // When
+        boolean isAvailable = room.isRoomAvailable(requestedArrivalDate, requestedDepartureDate);
+
+        // Then
+        assertTrue(isAvailable);
+    }
 }
