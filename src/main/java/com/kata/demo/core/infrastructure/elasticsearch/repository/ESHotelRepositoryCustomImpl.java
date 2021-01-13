853c165e8f193f4cdc8a2d730a13e1e9c50f08ef
kwerghi <k-werghi@artefrance.fr>
2021-01-13 18:15:32 +0100

    Implements elastic search repository

--- src/main/java/com/kata/demo/core/infrastructure/elasticsearch/repository/ESHotelRepositoryCustomImpl.java
+++ src/main/java/com/kata/demo/core/infrastructure/elasticsearch/repository/ESHotelRepositoryCustomImpl.java
@@ -1,9 +1,12 @@
 package com.kata.demo.core.infrastructure.elasticsearch.repository;
 
+import com.kata.demo.core.infrastructure.elasticsearch.model.ESHotel;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.query.Query;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ESHotelRepositoryCustomImpl implements ESHotelRepositoryCustom {
     private final ElasticsearchOperations operations;
@@ -14,6 +17,9 @@ public class ESHotelRepositoryCustomImpl implements ESHotelRepositoryCustom {
 
     @Override
     public List<String> searchWithin(Query query) {
-        return null;
+        return operations.search(query, ESHotel.class).get()
+                .map(SearchHit::getContent)
+                .map(ESHotel::getId)
+                .collect(Collectors.toList());
     }
 }
