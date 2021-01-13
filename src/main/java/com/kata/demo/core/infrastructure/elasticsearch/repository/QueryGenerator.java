d859e9c720984f427576daa3a41f1dbcdd402a5f
kwerghi <k-werghi@artefrance.fr>
2021-01-13 18:34:13 +0100

    Implement query generator

--- src/main/java/com/kata/demo/core/infrastructure/elasticsearch/repository/QueryGenerator.java
+++ src/main/java/com/kata/demo/core/infrastructure/elasticsearch/repository/QueryGenerator.java
@@ -1,10 +1,19 @@
 package com.kata.demo.core.infrastructure.elasticsearch.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.GeoDistanceOrder;
 import org.springframework.data.elasticsearch.core.query.Query;
 
 public class QueryGenerator {
     public static Query generateHotelSearchQuery(GeoPoint geoPoint, Double distance, String unit){
-        return null;
+        Query query = new CriteriaQuery(
+                new Criteria("location").within(geoPoint, distance.toString() + unit)
+        );
+        Sort sort = Sort.by(new GeoDistanceOrder("location", geoPoint).withUnit(unit));
+        query.addSort(sort);
+        return query;
     }
 }
